0f575e29e469420706ea9bce1ddb3faab76ab11c
Quantocquan <qle29210@gmail.com>
2023-03-03 21:39:18 +0700

    change

--- DoAnMonHocLTDT/src/Swing/GiaoDien.java
+++ DoAnMonHocLTDT/src/Swing/GiaoDien.java
@@ -2,13 +2,18 @@ package Swing;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Function.Graph;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JScrollBar;
@@ -17,13 +22,21 @@ import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 import java.awt.FlowLayout;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JLabel;
 
 public class GiaoDien extends JFrame  {
 
-	protected JButton btnLoadGraph, btnDuyetDothi, btnCheckConnect, prinImage;
-	protected JPanel contentPane, loadGraph, function;
-	JTextArea textAreaMatrix;
-	Controller controller = new Controller(this);
+	public JButton btnLoadGraph, btnDuyetDothi, btnCheckConnect, prinImage,btnRemove,btnAdd;
+	public JPanel contentPane, loadGraph, jPanelFunc;
+	public JTextArea textAreaMatrix;
+	public ActionListener controller;
+	public JTextField textFieldaddFirst;
+	public JTextField textFieldRemoveFirst;
+	public JTextField textFieldaddLast;
+	public JTextField textFieldRemoveLast;
+	public Graph graph;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -44,8 +57,11 @@ public class GiaoDien extends JFrame  {
 
 	/**
 	 * Create the frame.
+	 * @throws IOException 
+	 * @throws NumberFormatException 
 	 */
-	public GiaoDien() {
+	public GiaoDien() throws NumberFormatException, IOException {
+		graph = new Graph("E:\\CodeLTDT\\src\\graph.txt");
 		GUI();
 		addAction();
 	}
@@ -59,27 +75,59 @@ public class GiaoDien extends JFrame  {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
-		function = new JPanel();
-		contentPane.add(function);
-		function.setLayout(null);
+		jPanelFunc = new JPanel();
+		contentPane.add(jPanelFunc);
+		jPanelFunc.setLayout(null);
 
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] { "DFS", "BFS" }));
 		comboBox.setSelectedIndex(0);
-		comboBox.setBounds(157, 72, 141, 21);
-		function.add(comboBox);
+		comboBox.setBounds(151, 72, 141, 21);
+		jPanelFunc.add(comboBox);
 
 		prinImage = new JButton("Show graph");
 		prinImage.setBounds(10, 10, 119, 21);
-		function.add(prinImage);
+		jPanelFunc.add(prinImage);
 
 		btnCheckConnect = new JButton("Check connect");
 		btnCheckConnect.setBounds(10, 41, 141, 21);
-		function.add(btnCheckConnect);
+		jPanelFunc.add(btnCheckConnect);
 
 		btnDuyetDothi = new JButton("Graph browsing");
 		btnDuyetDothi.setBounds(10, 72, 119, 21);
-		function.add(btnDuyetDothi);
+		jPanelFunc.add(btnDuyetDothi);
+		
+		textFieldaddFirst = new JTextField();
+		textFieldaddFirst.setBounds(105, 103, 36, 21);
+		jPanelFunc.add(textFieldaddFirst);
+		textFieldaddFirst.setColumns(10);
+		
+		textFieldRemoveFirst = new JTextField();
+		textFieldRemoveFirst.setBounds(105, 131, 36, 20);
+		jPanelFunc.add(textFieldRemoveFirst);
+		textFieldRemoveFirst.setColumns(10);
+		
+		textFieldaddLast = new JTextField();
+		textFieldaddLast.setBounds(151, 103, 36, 21);
+		jPanelFunc.add(textFieldaddLast);
+		textFieldaddLast.setColumns(10);
+		
+		textFieldRemoveLast = new JTextField();
+		textFieldRemoveLast.setBounds(151, 131, 36, 20);
+		jPanelFunc.add(textFieldRemoveLast);
+		textFieldRemoveLast.setColumns(10);
+		
+		btnRemove = new JButton("RemoveEdge");
+		btnRemove.setBounds(10, 130, 91, 21);
+		jPanelFunc.add(btnRemove);
+		
+		btnAdd = new JButton("AddEdge");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAdd.setBounds(10, 103, 85, 21);
+		jPanelFunc.add(btnAdd);
 
 		loadGraph = new JPanel();
 		contentPane.add(loadGraph);
@@ -97,11 +145,43 @@ public class GiaoDien extends JFrame  {
 		btnLoadGraph = new JButton("Update graph");
 		btnLoadGraph.setBounds(344, 12, 118, 21);
 		loadGraph.add(btnLoadGraph);
+		
+	    controller = new Controller(this);
 	}
 	public void addAction() {
 		this.btnLoadGraph.addActionListener(controller);
+		this.btnAdd.addActionListener(controller);
+		this.btnRemove.addActionListener(controller);
 	}
-	
-
-	
+	/**
+	 * phuong thuc in ra ma tran tren textArea
+	 */
+	public void addSetTextAreaMatrix() {
+		this.textAreaMatrix.setText(graph.printMatrix());
+	}
+	/**
+	 * phuong thuc them canh 
+	 */
+	public void addEdge() {
+		int first = Integer.valueOf(textFieldaddFirst.getText());
+		int last = Integer.valueOf(textFieldaddLast.getText());
+		if(first < 0 || first >= graph.soDinh || last < 0 || last >= graph.soDinh)  {
+			JOptionPane.showMessageDialog(btnRemove, "Ban chi co the nhap index dinh dau va dinh cuoi trong khoang quy dinh");
+		}
+		graph.addEdges(first, last);
+	}
+	/**
+	 * phuong thuc xoa canh
+	 */
+	public void removeEdge() {
+		int first = Integer.valueOf(textFieldRemoveFirst.getText());
+		int last = Integer.valueOf(textFieldRemoveLast.getText());
+		if(first < 0 || first >= graph.soDinh || last < 0 || last >= graph.soDinh)  {
+			JOptionPane.showMessageDialog(btnRemove, "Ban chi co the nhap index dinh dau va dinh cuoi trong khoang quy dinh");
+		}else if(graph.mtk[first][last] == 0 ) {
+			JOptionPane.showMessageDialog(btnRemove, "Canh can xoa khong ton tai");
+		}
+			
+		graph.removeEdges(first, last);
+	}
 }
