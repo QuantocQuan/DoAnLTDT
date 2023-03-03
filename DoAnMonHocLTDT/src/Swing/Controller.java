2290fc27fe6aa4c7e588873af7418fd1adb956af
Quantocquan <qle29210@gmail.com>
2023-03-03 21:38:54 +0700

    change

--- DoAnMonHocLTDT/src/Swing/Controller.java
+++ DoAnMonHocLTDT/src/Swing/Controller.java
@@ -1,34 +1,34 @@
 package Swing;
 
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import Function.Graph;
 
-public class Controller implements ActionListener{
-	protected GiaoDien giaoDien ;
-	protected Graph graphFunction ;
-	
+public class Controller implements ActionListener {
+	GiaoDien giaoDien;
+	Graph graph;
+
 	public Controller(GiaoDien giaoDien) {
-		super();
 		this.giaoDien = giaoDien;
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource() == giaoDien.btnLoadGraph) {
-			// Code To popup an ERROR_MESSAGE Dialog.
-			try {
-				this.graphFunction = new Graph("C:\\Users\\ASUS\\Downloads\\New folder\\DoAnLTDT\\DoAnMonHocLTDT\\graph.txt");
-			} catch (NumberFormatException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			giaoDien.textAreaMatrix.setText(graphFunction.printMatrix());
+		String ac = e.getActionCommand();
+
+		if (ac.equals("Update graph")) {
+			giaoDien.addSetTextAreaMatrix();
+		}
+		if (ac.equals("AddEdge")) {
+			giaoDien.addEdge();
+
+		}
+		if (ac.equals("RemoveEdge")) {
+			giaoDien.removeEdge();
+
 		}
 	}
 
