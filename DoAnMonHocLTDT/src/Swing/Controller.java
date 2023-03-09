0a3cb5c47007ba8d0133dd8499256bb695b3415a
LeLong <21130436@st.hcmuaf.edu.vn>
2023-03-09 20:14:17 +0700

    ex

--- DoAnMonHocLTDT/src/Swing/Controller.java
+++ DoAnMonHocLTDT/src/Swing/Controller.java
@@ -14,7 +14,6 @@ import Function.Graph;
 public class Controller implements ActionListener {
 	View giaoDien;
 
-
 	public Controller(View giaoDien) {
 		this.giaoDien = giaoDien;
 	}
@@ -27,35 +26,49 @@ public class Controller implements ActionListener {
 			this.giaoDien.textAreaMatrix.setText(giaoDien.graph.printMatrix());
 		}
 		if (ac.equals("AddEdge")) {
-			int first = Integer.valueOf(giaoDien.textFieldFirst.getText());
-			int last = Integer.valueOf(giaoDien.textFieldLast.getText());
-			if (first < 0 || first >= giaoDien.graph.soDinh || last < 0 || last >= giaoDien.graph.soDinh) {
-				JOptionPane.showMessageDialog(giaoDien, "Bạn chỉ có thể nhập  0 <= đỉnh < " + giaoDien.graph.soDinh,	"Error", JOptionPane.ERROR_MESSAGE);
+
+			try {
+				int first = Integer.valueOf(giaoDien.textFieldFirst.getText());
+				int last = Integer.valueOf(giaoDien.textFieldLast.getText());
+				if (first < 0 || first >= giaoDien.graph.soDinh || last < 0 || last >= giaoDien.graph.soDinh) {
+					JOptionPane.showMessageDialog(giaoDien, "Bạn chỉ có thể nhập  0 <= đỉnh < " + giaoDien.graph.soDinh,
+							"Error", JOptionPane.ERROR_MESSAGE);
+				}
+				giaoDien.graph.addEdges(first, last);
+				this.giaoDien.textAreaMatrix.setText(giaoDien.graph.printMatrix());
+			} catch (NumberFormatException c) {
+				JOptionPane.showMessageDialog(giaoDien, "Không được để trống đỉnh đầu và đỉnh cuối cần thêm", "Error",
+						JOptionPane.ERROR_MESSAGE);
 			}
-			giaoDien.graph.addEdges(first, last);
-			this.giaoDien.textAreaMatrix.setText(giaoDien.graph.printMatrix());
 		}
 		if (ac.equals("RemoveEdge")) {
-			int first = Integer.valueOf(giaoDien.textFieldFirst.getText());
-			int last = Integer.valueOf(giaoDien.textFieldLast.getText());
-			if (first < 0 || first >= giaoDien.graph.soDinh || last < 0 || last >= giaoDien.graph.soDinh) {
-				JOptionPane.showMessageDialog(giaoDien, "Bạn chỉ có thể nhập  0 <= đỉnh < " + giaoDien.graph.soDinh,	"Error", JOptionPane.ERROR_MESSAGE);
-			} else if (giaoDien.graph.mtk[first][last] == 0) {
-				JOptionPane.showMessageDialog(giaoDien, "Cạnh cần xóa không tồn tại", "Error", JOptionPane.ERROR_MESSAGE);
+			try {
+				int first = Integer.valueOf(giaoDien.textFieldFirst.getText());
+				int last = Integer.valueOf(giaoDien.textFieldLast.getText());
+				if (first < 0 || first >= giaoDien.graph.soDinh || last < 0 || last >= giaoDien.graph.soDinh) {
+					JOptionPane.showMessageDialog(giaoDien, "Bạn chỉ có thể nhập  0 <= đỉnh < " + giaoDien.graph.soDinh,
+							"Error", JOptionPane.ERROR_MESSAGE);
+				} else if (giaoDien.graph.mtk[first][last] == 0) {
+					JOptionPane.showMessageDialog(giaoDien, "Cạnh cần xóa không tồn tại", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					giaoDien.graph.removeEdges(first, last);
+					this.giaoDien.textAreaMatrix.setText(giaoDien.graph.printMatrix());
+				}
+			} catch (NumberFormatException c) {
+				JOptionPane.showMessageDialog(giaoDien, "Không được để trống đỉnh đầu và đỉnh cuối cần xóa", "Error",
+						JOptionPane.ERROR_MESSAGE);
 			}
 
-			giaoDien.graph.removeEdges(first, last);
-			this.giaoDien.textAreaMatrix.setText(giaoDien.graph.printMatrix());
 		}
-		if(ac.equals("Choose file")) {
+		if (ac.equals("Choose file")) {
 			giaoDien.fileChooser = new JFileChooser("E:\\DoAnLTDT\\DoAnMonHocLTDT");
 			int returnVal = giaoDien.fileChooser.showOpenDialog(giaoDien);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-	            File file =giaoDien.fileChooser.getSelectedFile();
-	            //This is where a real application would open the file.
-	            System.out.println(file.toString());
-	            giaoDien.pathFile = file.toString();
-	            try {
+				File file = giaoDien.fileChooser.getSelectedFile();
+				// This is where a real application would open the file.
+				System.out.println(file.toString());
+				giaoDien.pathFile = file.toString();
+				try {
 					giaoDien.graph = new Graph(giaoDien.pathFile);
 				} catch (NumberFormatException e1) {
 					// TODO Auto-generated catch block
@@ -64,19 +77,28 @@ public class Controller implements ActionListener {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	            
-	            
-	        } 
+
+			}
 		}
 		if (ac.equals("Graph browsing")) {
+			int start;
+			String strStart = giaoDien.textFieldDinhChon.getText();
 			System.out.println("Ban vua nhan nut");
+			if (!strStart.equals(""))
+				start = Integer.valueOf(strStart);
+
+			else
+				start = 0;
+			System.out.printf("Đỉnh đầu là %d", start);
 			if (giaoDien.rdbtnDFS.isSelected()) {
-				int start = Integer.valueOf(giaoDien.textFieldDinhChon.getText());
-				JOptionPane.showMessageDialog(giaoDien, "Duyệt đồ thị theo DFS: " + giaoDien.graph.DFSLinkkeList(start));
+
+				JOptionPane.showMessageDialog(giaoDien,
+						"Duyệt đồ thị theo DFS: " + giaoDien.graph.DFSLinkkeList(start));
 
 			} else if (giaoDien.rdbtnBFS.isSelected()) {
-				int start = Integer.valueOf(giaoDien.textFieldDinhChon.getText());
-				JOptionPane.showMessageDialog(giaoDien, "Duyệt đồ thị theo BFS: " + giaoDien.graph.BFSLinkedlist(start));
+
+				JOptionPane.showMessageDialog(giaoDien,
+						"Duyệt đồ thị theo BFS: " + giaoDien.graph.BFSLinkedlist(start));
 			}
 		}
 	}
